3bf8254c5aa86b108c9678eb0fc98ee9d7468ebd
khassimo <aboulkasimo908@gmail.com>
2025-12-05 15:43:46 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ app = Flask(__name__, template_folder="templates")
 @app.get("/")
 def home():
     return render_template("index.html")
+@app.get("/index")
+def home():
+    return render_template("index.html")
 
 
 if __name__== "__main__":
